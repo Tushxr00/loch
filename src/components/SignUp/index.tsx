bda7c7235705cdebc1c397cc2a8040a4d43bd8ef
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-12-09 09:29:23 +0530

    FIX: mobile responsive sign up page fixed

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -6,9 +6,9 @@ const SignUp = () => {
   const { SignupSchema, onSubmit,isEmailSent } = useSignUp();
   console.log({ SignupSchema, onSubmit });
   return (
-    <div className="flex justify-center items-center h-full w-full">
+    <div className="flex justify-center items-center h-full w-full px-3 sm:px-0">
       <div className="max-w-fit ">
-        <h1 className="text-[#B0B1B3] text-4xl mb-8">
+        <h1 className="text-[#B0B1B3] text-3xl  sm:text-4xl mb-8">
           Sign up for
           <br />
           exclusive access.
@@ -30,14 +30,14 @@ const SignUp = () => {
               {(errors.email && touched.email) ? <div className="mt-2 ml-2 text-sm text-red-500 font-medium">{errors.email}</div> : null}
               <button
                 type="submit"
-                className="font-semibold w-[350px] h-[59px] bg-black text-white rounded-lg px-7 py-4 mx-auto mt-6"
+                className="font-semibold w-full sm:w-[350px] h-[59px] bg-black text-white rounded-lg px-7 py-4 mx-auto mt-6"
               >
                 Get started
               </button>
             </Form>
           )}
         </Formik>
-          <p className="text-center py-4 mt-6 relative">You’ll receive an email with an invite link to join.</p>
+          <p className="text-center py-4 mt-6 relative ">You’ll receive an email with an invite link to join.</p>
           {(isEmailSent) && <p className="text-center bg-green-100 font-medium text-base rounded mt-2 text-green-900">
           Email sent
         </p>}
